c5e19a507cbab43ace8b657edf8f19b1449abc51
KishalJayalath <kishalsj2002@gmail.com>
2025-04-29 17:32:57 +0530

    updated  landing page
    updated  landing page

--- src/components/three/SceneObjects.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { useGLTF } from '@react-three/drei';
-import * as THREE from 'three';
-
-// This file would contain more complex 3D models when implemented with actual models
-// For now, we're using the placeholder components from ThreeCanvas.tsx
